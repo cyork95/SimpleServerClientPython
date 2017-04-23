b523348b2d07e0b3e6744352c96ac66139557429
Cody York <(clyork@bsu.edu)>
2017-04-23 16:16:02 -0400

    Fixed the obvious errors

---
+++ client2.py
@@ -0,0 +1,15 @@
+from socket import *
+
+
+serverName = '10.4.82.254'
+
+serverName = '127.0.0.1'
+serverPort = 12000
+clientSocket = socket (AF_INET,SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+
+while True:
+    message = str("Enter message: ")
+    clientSocket.sendall(message)
+    response = clientSocket.recv(1024)
+    print (response)
